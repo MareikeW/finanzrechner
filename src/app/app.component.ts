b3a3dd7025c979cd819f767a61b48f9d959088a0
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-07-01 10:56:25 +0200

    Headerstyling ab Tablet hinzugefügt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   isMenuOpen: boolean = false;
 
-  toggleMenu() {
+  toggleMenu() { 
     this.isMenuOpen ? this.isMenuOpen = false : this.isMenuOpen = true;
   }
 }
