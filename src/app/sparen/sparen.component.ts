79d00f13fc1ea20f698864c975309cc360dc9ee3
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-07-02 11:24:54 +0200

    Monatliche Sparraten - Korrekte Rechnung

--- src/app/sparen/sparen.component.ts
+++ src/app/sparen/sparen.component.ts
@@ -22,7 +22,7 @@ export class SparenComponent {
   berechneErgebnis() {
     if (this.sparintervall.value === "monatlich") {
     // Monatliche Einzahlungen am Anfang des Monats
-    /* 
+    /* Mit monatlicher Verzinsung
       P = 5000. PMT = 100. r = 5/100 = 0.05 (decimal). n = 12. t = 10.
       Total = [ P(1+r/n)^(nt) ] + [ PMT × (((1 + r/n)^(nt) - 1) / (r/n)) × (1+r/n) ]
       Total = [ 5000 (1 + 0.05 / 12) ^ (12 × 10) ] + [ 100 × (((1 + 0.00416)^(12 × 10) - 1) / (0.00416)) × (1 + 0.05 / 12) ]
@@ -32,10 +32,10 @@ export class SparenComponent {
     this.ergebnis = this.zinsesZinsAnfangskapital + this.zinsesZins;
     } else if (this.sparintervall.value === "jährlich") {
       // Jährliche Einzahlungen am Anfang des Jahres
-    /* 
+    /* Mit jährlicher Verzinsung
       P = 5000. PMT = 100. r = 5/100 = 0.05 (decimal). n = 1. t = 10.
       Total = [ P(1+r/n)^(nt) ] + [ PMT × (((1 + r/n)^(nt) - 1) / (r/n)) × (1+r/n) ]
-      Total = [ 5000 (1 + 0.05 / 1) ^ (1 × 10) ] + [ 100 × (((1 + 0.00416)^(1 × 10) - 1) / (0.00416)) + (1 + 0.05 / 1) ]
+      Total = [ 5000 (1 + 0.05 / 1) ^ (1 × 10) ] + [ 100 × (((1 + 0.00416)^(1 × 10) - 1) / (0.00416)) * (1 + 0.05 / 1) ]
   	*/
     this.zinsesZinsAnfangskapital = this.anfangskapital.value * ((1 + ((this.zinssatz.value / 100) / 1))**(1 * this.ansparzeit.value));
     this.zinsesZins = this.sparrate.value * (((1 + ((this.zinssatz.value / 100) / 1))**(1 * this.ansparzeit.value) - 1) / ((this.zinssatz.value / 100) / 1)) * (1 + ((this.zinssatz.value / 100) / 1));
